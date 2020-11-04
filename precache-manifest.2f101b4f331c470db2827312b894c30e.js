1caa34ab3b45078802eeeae48c668e591be15c85
Zach Tollefson <tolle239@umn.edu>
2020-11-04 07:42:34 -0600

    Updates

---
+++ precache-manifest.2f101b4f331c470db2827312b894c30e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3de851d3e8ed481bb268f8d7173293cf",
+    "url": "/kanban-board/index.html"
+  },
+  {
+    "revision": "570971dcd922e1d7c2c9",
+    "url": "/kanban-board/static/css/main.2d53085b.chunk.css"
+  },
+  {
+    "revision": "d65b536a84b1e1cb732c",
+    "url": "/kanban-board/static/js/2.f2507dab.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/kanban-board/static/js/2.f2507dab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "570971dcd922e1d7c2c9",
+    "url": "/kanban-board/static/js/main.ef39696f.chunk.js"
+  },
+  {
+    "revision": "ba2096fa3b770a19f0f1",
+    "url": "/kanban-board/static/js/runtime-main.8e469a6a.js"
+  }
+]);
